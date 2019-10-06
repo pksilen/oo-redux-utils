dcd0b0b9feaacc3e5d14d600f540bf12fcb18355
Petri Silen <petri.silen@silensoft.com>
2019-10-06 11:43:34 +0300

    Modified OOReduxUtils export

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // @flow
 
-export { default as OOReduxUtils } from './OOReduxUtils';
+export { default } from './OOReduxUtils';
 export { default as AbstractAction } from './AbstractAction';
 export type { ActionObject, Dispatch, DispatchWrapper } from './DispatchWrapper';
