4de3d0d89d0eb79a7dbfd6cf7e75b334fca134b1
Petri Silen <petri.silen@silensoft.com>
2019-10-16 12:56:07 +0300

    Updated

--- src/AbstractComponent.js
+++ src/AbstractComponent.js
@@ -4,9 +4,12 @@ import React from 'react';
 import type { InExactDispatchWrapper } from './DispatchWrapper';
 import AbstractAction from './AbstractAction';
 
-export default class AbstractComponent<PropsType: InExactDispatchWrapper, StateType> extends React.Component<PropsType, StateType> {
+export default class AbstractComponent<PropsType: InExactDispatchWrapper, StateType> extends React.Component<
+  PropsType,
+  StateType
+> {
   dispatch(action: AbstractAction<any>) {
     const { dispatch } = this.props;
-    dispatch({ type: action});
+    dispatch({ type: action });
   }
 }
