883ac502a3fe9a95d41afe5ed6a25a4238bc0452
Petri Silen <petri.silen@silensoft.com>
2019-10-22 16:39:04 +0300

    Refactored

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ export { default } from './OOReduxUtils';
 export { default as AbstractAction } from './AbstractAction';
 export { default as AbstractComponent } from './AbstractComponent';
 export { DispatchAction, DispatchActionToComponentType } from './DispatchUtils';
-export type { DispatchWrapper } from './DispatchWrapper';
+export type { Dispatch, DispatchWrapper } from './DispatchWrapper';
