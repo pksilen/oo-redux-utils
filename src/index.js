7edaebeae4a2e0de6f946932b942c41befe74b78
Petri Silen <petri.silen@silensoft.com>
2019-11-09 13:16:58 +0200

    Updated

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ export { default as AbstractAction } from './AbstractAction';
 export { default as NoneAction } from './NoneAction';
 export { default as AbstractComponent } from './AbstractComponent';
 export { default as DispatchUtils } from './DispatchUtils';
-export { default as ControllerFactory } from './AbstractControllerFactory';
+export { default as AbstractControllerFactory } from './AbstractControllerFactory';
 export type { DispatchAction, DispatchActionToComponentType } from './DispatchUtils';
 export type { Dispatch, DispatchWrapper } from './DispatchWrapper';
