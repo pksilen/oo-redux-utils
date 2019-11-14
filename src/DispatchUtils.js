b918e8b5abe940e66f5bb6b953d6268b5c2f0807
Petri Silen <petri.silen@silensoft.com>
2019-11-14 11:27:42 +0200

    Modified

--- src/DispatchUtils.js
+++ src/DispatchUtils.js
@@ -5,7 +5,9 @@ import AbstractAction from './AbstractAction';
 import type { Dispatch } from './DispatchWrapper';
 
 export type DispatchAction = (AbstractAction<any>) => void;
-export type DispatchActionToComponentType = (AbstractAction<any>, React.ComponentType<any>) => void;
+export type DispatchActionWrapper = {
+  dispatchAction: DispatchAction
+}
 
 export default class DispatchUtils {
   static createActionDispatcher(dispatch: Dispatch): DispatchAction {
--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ export { default as AbstractAction } from './AbstractAction';
 export { default as NoneAction } from './NoneAction';
 export { default as DispatchUtils } from './DispatchUtils';
 export { default as AbstractControllerFactory } from './AbstractControllerFactory';
-export type { DispatchAction, DispatchActionToComponentType } from './DispatchUtils';
+export type { DispatchAction, DispatchActionWrapper } from './DispatchUtils';
 export type { Dispatch, DispatchWrapper } from './DispatchWrapper';
 export type { Controller } from './Controller';
