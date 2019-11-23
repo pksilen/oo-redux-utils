b17cef21ad9c60b5d1f9e499bdadfc7de65e8436
Petri Silen <petri.silen@silensoft.com>
2019-11-23 15:14:31 +0200

    Refactoring

--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ export { default } from './OOReduxUtils';
 export { default as AbstractAction } from './AbstractAction';
 export { default as DispatchUtils } from './DispatchUtils';
 export { default as ControllerFactory } from './ControllerFactory';
+export { default as createNamespacedReducers } from './createNamespacedReducers';
 export type { DispatchAction, DispatchActionWrapper } from './DispatchUtils';
 export type { Dispatch } from './Dispatch';
+
