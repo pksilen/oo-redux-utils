4b06cab41ddc90cf29f9963c0bb0ccdde339bdb8
Petri Silen <petri.silen@silensoft.com>
2019-11-28 16:43:06 +0200

    Refactoring

--- src/DispatchUtils.js
+++ src/DispatchUtils.js
@@ -4,14 +4,14 @@ import * as React from 'react';
 import AbstractAction from './AbstractAction';
 import type { Dispatch } from './Dispatch';
 
-export type DispatchAction = (AbstractAction<any>) => void;
+export type DispatchAction = (AbstractAction<any, any>) => void;
 export type DispatchActionWrapper = {
   dispatchAction: DispatchAction
 }
 
 export default class DispatchUtils {
   static createActionDispatcher(dispatch: Dispatch): DispatchAction {
-    return function(action: AbstractAction<any>) {
+    return function(action: AbstractAction<any, any>) {
       dispatch({ type: action });
     };
   }
