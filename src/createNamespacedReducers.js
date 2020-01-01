10a042966163668e3c0f73ba367c59fc4ad5284e
Petri Silen <petri.silen@silensoft.com>
2020-01-01 13:31:57 +0200

    Refactoring

--- src/createNamespacedReducers.js
+++ src/createNamespacedReducers.js
@@ -2,15 +2,17 @@
 
 import type { ActionObject } from './Dispatch';
 
+type Reducers<StateNamespacesType: { [string]: string }, StateType> = {
+  [$Keys<StateNamespacesType>]: (StateType | void, ActionObject) => StateType
+};
+
 export default function<StateNamespacesType: { [string]: string }, StateType>(
   stateNamespaces: StateNamespacesType,
   createNamespacedStateReducer: ($Keys<StateNamespacesType>) => (StateType | void, ActionObject) => StateType
-) {
+): Reducers<StateNamespacesType, StateType> {
   return Object.keys(stateNamespaces).reduce(
     (
-      accumulatedReducers: {
-        [$Keys<StateNamespacesType>]: (StateType | void, ActionObject) => StateType
-      },
+      accumulatedReducers: Reducers<StateNamespacesType, StateType>,
       stateNamespace: $Keys<StateNamespacesType>
     ) => ({ ...accumulatedReducers, [stateNamespace]: createNamespacedStateReducer(stateNamespace) }),
     {}
